ffe1727284a4c7d78e80c6b2db99afbcf31b1fc0
WiLLiAM111333 <willjoh03@hotmail.com>
2022-08-06 16:24:53 +0200

    bugfix

--- lib/VladimirClient.ts
+++ lib/VladimirClient.ts
@@ -1,4 +1,4 @@
-import { Client, ClientOptions, Options } from "discord.js";
+import { Client, ClientOptions } from "discord.js";
 import { CommandHandler } from "./command/handler/CommandHandler";
 import { EventHandler } from "./event/EventHandler";
 import { ModerationLogger } from "./logger/ModerationLogger";
@@ -9,26 +9,6 @@ export class VladimirClient extends Client {
   public moderationLogger: ModerationLogger;
 
   public constructor(options: ClientOptions) {
-    options.makeCache = Options.cacheWithLimits({
-      ApplicationCommandManager: 0,
-      BaseGuildEmojiManager: 0,
-      GuildEmojiManager: 0,
-      GuildMemberManager: 0,
-      GuildBanManager: 0,
-      GuildInviteManager: 0,
-      GuildScheduledEventManager: 0,
-      GuildStickerManager: 0,
-      MessageManager: 0,
-      PresenceManager: 0,
-      ReactionManager: 0,
-      ReactionUserManager: 0,
-      StageInstanceManager: 0,
-      ThreadManager: 0,
-      ThreadMemberManager: 0,
-      UserManager: 0,
-      VoiceStateManager: 0
-    });
-
     super(options);
 
     this.commandHandler = new CommandHandler(this);
